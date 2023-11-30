db947fe62631e0590d065bf22da5053aaa88c359
Ronen Boneh <rboneh@live.com>
2023-11-30 16:10:45 +0200

    Split index.js to index.js and utilities.js + some cleaning

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
+import * as u from './public/utilities.js';
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 // console.log(__dirname + "/public/index.html");
 
@@ -54,75 +56,18 @@ app.get("/contact", (req, res) => {
 
 app.get("/photography", (req, res) => {
   // console.log('manage /photo');
-  const fileList = getFiles(__dirname + "/views/blogs/photography");
-  const tableBody = makeTableBody(fileList);
+  const fileList = u.getFiles(__dirname + "/views/blogs/photography");
+  const tableBody = u.makeTableBody(fileList);
   res.render("partials/index.ejs", { tableBody: tableBody, themeImage: "https://picsum.photos/id/91/800/200?random=1" });
 });
 
 app.get("/blog-form", (req, res) => {
-  console.log(__dirname + "/public/blog-form.html");
-  console.log("Sending blog-form.html");
-  res.sendFile(__dirname + "/public/blog-form.html");
+  const filePath = __dirname + "/views/partials/blog-form.ejs";
+  const blogForm = u.readFileSynchronously(filePath);
+  res.render("partials/index.ejs", { blogForm: blogForm, themeImage: "https://picsum.photos/id/180/800/100?random=1"});
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-//////////////////////////////////////////////
-// Recursive function to get files
-function getFiles(dir, files = []) {
-  // Get an array of all files and directories in the passed directory using fs.readdirSync
-  const fileList = fs.readdirSync(dir);
-  // Create the full path of the file/directory by concatenating the passed directory and file/directory name
-  for (const file of fileList) {
-    const name = `${dir}/${file}`;
-    // Check if the current file/directory is a directory using fs.statSync
-    if (fs.statSync(name).isDirectory()) {
-      // If it is a directory, recursively call the getFiles function with the directory path and the files array
-      getFiles(name, files);
-    } else {
-      // If it is a file, push the full path to the files array
-      files.push(name);
-    }
-  }
-  return files;
-}
-
-function readFileSynchronously(filePath) {
-  try {
-    const fileContent = fs.readFileSync(filePath, "utf8");
-    // console.log(fileContent);
-    return fileContent;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function makeTableBody(fileList) {
-  // const fileContent = readFileSynchronously(fileList[0]);
-  let lines = [];
-  let line = "";
-  let tableBody = "";
-
-  for (let fn in fileList) {
-    let fileContent = readFileSynchronously(fileList[fn]);
-    lines = fileContent.split("\n");
-    line = makeTableLine(lines.slice(0, 1), lines[1], lines[2]);
-    // console.log(line);
-    tableBody += line;
-  }
-  return tableBody;
-}
-
-function makeTableLine(title, date, author) {
-  const line = `
-      <tr>
-        <td><a href="./blogs/photography/blog_template.txt"></a>${title}</td>
-        <td>${date}</td>
-        <td>${author}</td>
-        <td><button type="button" class="btn btn-light btn-delete">Delete</button></td>
-      </tr>`;
-
-  return line;
-}
